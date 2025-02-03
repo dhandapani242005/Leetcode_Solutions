159493cc5d458fb7075af95f650450650a8453f2
Dhandapani M <157560061+dhandapani242005@users.noreply.github.com>
2025-02-03 17:11:41 +0530

    Time: 0 ms (100%), Space: 44.7 MB (12.75%) - LeetHub

--- 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.java
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.java
@@ -3,15 +3,14 @@ class Solution {
         if (root == null) {
             return null;
         }
-        //if the range is correct then checking both root left and right
         if (root.val >= L && root.val <= R) {
-            root.left = trimBST(root.left, L, R);//Trim the left subtree
-            root.right = trimBST(root.right, L, R);//Trim the right subtree
+            root.left = trimBST(root.left, L, R);
+            root.right = trimBST(root.right, L, R);
         } else if (root.val < L) {
-            //if the root val is less than low then getting values from left will be even lower(binary tree rule) so we need to set the root to root.right;
+            
             root = trimBST(root.right, L, R);
         } else if (root.val > R) {
-            //if the root val is greater than high then getting values from right will be even higher(binary tree rule) so we need to the root to root.left.
+            
             root = trimBST(root.left, L, R);
         }
         return root;
