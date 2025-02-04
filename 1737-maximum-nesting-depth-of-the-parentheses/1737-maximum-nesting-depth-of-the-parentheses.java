73d68f112243f673289d23e3ea753fb9f9a77ea8
Dhandapani M <157560061+dhandapani242005@users.noreply.github.com>
2025-02-04 13:41:34 +0530

    Time: 0 ms (100%), Space: 41.7 MB (43.4%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.java
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.java
@@ -16,7 +16,7 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.maxDepth("(1+(2*3)+((8)/4))+1")); // Output: 3
-        System.out.println(solution.maxDepth("(1)+((2))+(((3)))")); // Output: 3
+        //System.out.println(solution.maxDepth("(1+(2*3)+((8)/4))+1")); // Output: 3
+        //System.out.println(solution.maxDepth("(1)+((2))+(((3)))")); // Output: 3
     }
 }
