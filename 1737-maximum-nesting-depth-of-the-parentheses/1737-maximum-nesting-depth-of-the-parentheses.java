d2a6021ce83b2a1e7434a83d17c03779211c0188
Dhandapani M <157560061+dhandapani242005@users.noreply.github.com>
2025-02-04 13:37:35 +0530

    Time: 0 ms (100%), Space: 41.3 MB (91.25%) - LeetHub

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxDepth(String s) {
+        int maxDepth = 0, currentDepth = 0;
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                currentDepth++;
+                maxDepth = Math.max(maxDepth, currentDepth);
+            } else if (c == ')') {
+                currentDepth--;
+            }
+        }
+        
+        return maxDepth;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maxDepth("(1+(2*3)+((8)/4))+1")); // Output: 3
+        System.out.println(solution.maxDepth("(1)+((2))+(((3)))")); // Output: 3
+    }
+}
